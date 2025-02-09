5fc70bf1674966b817893f47fcf240cd506998df
Vinicius Rezende <pereiravinicius0537@gmail.com>
2025-02-09 10:48:41 -0300

    Definindo as configurações iniciais da api do deepseek

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 import config
 
 def main():
-  env_config = config.load_config()
-  print(f"API do DeepSeek: {env_config.deepseek_api}")
-  print(f"url do suap: {env_config.suap_url}")
-  print(f"usuário: {env_config.user}")
-  print(f"Senha: {env_config.password}")
+  setup = config.load_config()
+  deepseek = config.deepseek()
+
+  print(deepseek.response("oi, tudo bem?"))
+
 
 if __name__ == "__main__":
   main()
